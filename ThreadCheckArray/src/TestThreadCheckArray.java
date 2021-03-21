f7cdfd6c74f52cf06ff26f64be5d2c1d98cee5a9
wafagh <wafa-@LAPTOP-9THQ9EL0>
2021-03-21 12:12:44 +0200

    test upgrades

--- ThreadCheckArray/src/TestThreadCheckArray.java
+++ ThreadCheckArray/src/TestThreadCheckArray.java
@@ -8,13 +8,14 @@ public class TestThreadCheckArray {
 	public static void main(String[] args) {
 		try (Scanner input = new Scanner(System.in)) {
 			Thread thread1, thread2;
-			System.out.println("Enter array size");
-			int num  = input.nextInt();
-			int [] array = new int[num];
-			System.out.println("Enter numbers for array");
+			System.out.println("Enter the numbers");
+			/*int num  = input.nextInt();
+			int [] array = new int[num];*/
+			while (input.hasNextint()) {
+                array.add(input.next());
+            }
 			
-			for (int index = 0; index < num; index++) 
-				array[index] = input.nextInt();
+			
 			
 			System.out.println("Enter number");
 			num = input.nextInt();
@@ -41,7 +42,8 @@ public class TestThreadCheckArray {
 			}
 			System.out.println("Solution for b : " + sd.getB() + ",n = " + sd.getArray().length);
 			System.out.print("I:    ");
-			for(int index = 0; index < sd.getArray().length ; index++)
+			
+			for(int index = 0; index < array.size() ; index++)
 				System.out.print(index + "    ");
 			System.out.println();
 			System.out.print("A:    ");
